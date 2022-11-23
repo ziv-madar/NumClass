140479e9a49af0a067f74cf2af76810c46b4ddef
zurizack <86063016+zurizack@users.noreply.github.com>
2022-11-24 00:25:40 +0200

    update
    commits

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,10 +1,19 @@
 #include "NumClass.h"
 #include <math.h>
 
+/*
+get a num and his num of digits
+take the unity digit from the number and power of his num of digit
+then divide the num by 10 and call recorsy
+sum the result evry time
+*\
 int armstrong(int n, int digits) {
     return (n > 0) ? pow(n % 10, digits) + armstrong(n / 10, digits) : 0;
 }
 
+/*
+take the result from armstrong and check if the same with original num
+*\
 int isArmstrong(int n) {
     return n == armstrong(n, ((int)log10(n)) + 1);
 }
