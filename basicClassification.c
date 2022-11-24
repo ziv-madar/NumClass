2aba30d17ad5c8eedf7abb56c52dfd4f1f3d3908
zurizack <86063016+zurizack@users.noreply.github.com>
2022-11-24 14:15:30 +0200

    commit

--- basicClassification.c
+++ basicClassification.c
@@ -1,5 +1,7 @@
 #include "NumClass.h"
-
+/*
+do factorial.. Multiply the number by all the numbers smaller than it
+*/
 int factorial(int n) {
     int res = 1;
     for (int i = 1; i <= n; i++) {
@@ -8,10 +10,19 @@ int factorial(int n) {
     return res;
 }
 
+/*
+
+*/
 int isPrime(int n) {
+    
+    /*Extreme case*/
     if (n == 0 || n == 1)
         return 1;
-
+    
+    /*
+    Passes over half of the first members that are smaller than it
+    and chek if one of them divide the original num without a remainder
+    */
     for (int i = 2; i <= n / 2; i++) {
         if (n % i == 0) {
             return 0;
@@ -20,6 +31,11 @@ int isPrime(int n) {
     return 1;
 }
 
+/*
+send evry digits to factorial
+take the sum of the digits after use factorial
+check if equal to original num
+*/
 int isStrong(int n) {
     int sum = 0, original = n;
     while (n > 0) {
