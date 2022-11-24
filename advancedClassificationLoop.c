8e1b298596677378bd5548f61106bf96c7c2ecfb
zurizack <86063016+zurizack@users.noreply.github.com>
2022-11-24 14:02:08 +0200

    commit

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -2,10 +2,19 @@
 #include <math.h>
 
 int isArmstrong(int n) {
+    /*
+    sum = sum of the uniuty after rais it by its num of digit
+    original = save the original num to compare later
+    digits = the number of digits
+    */
     int sum = 0,
         original = n,
         digits = ((int) log10(n)) + 1;
-
+    
+    /*
+    take the unity digit from num rais it by digits
+    do it again to num/10 till num = 0
+    */
     while (n > 0) {
         sum += pow(n % 10, digits);
         n = n / 10;
@@ -13,9 +22,22 @@ int isArmstrong(int n) {
     return original == sum;
 }
 
-int isPalindrome(int n) {
-    int remainder, temp = n, sum = 0;
+/*
 
+*/
+int isPalindrome(int n) {
+    /*
+    remainder = the curent digit
+    sum = the revers num
+    */
+    int remainder, temp = n, sum = 0;
+    
+    /*
+    take the unity digit in to the sum
+    Multiply the number by 10 and sum with current unity digit
+    divide the num by 10 and do all over again
+    till the num = 0
+    */
     while (temp > 0) {
         remainder = temp % 10;
         sum = (sum * 10) + remainder;
